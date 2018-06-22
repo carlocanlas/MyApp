47e396a6fa5d53a73eec36a0be7883d904871bca
Carlo Canlas <da.carlo.canlas@asia.ing.com>
2018-06-22 09:47:46 +0800

    added alert in button onClick

--- app/src/main/java/com/example/dan/myapp/MainActivity.java
+++ app/src/main/java/com/example/dan/myapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.dan.myapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -9,6 +11,17 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
         Log.i("Info", "Button was pressed");
+
+        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+        alertDialog.setTitle("Hey");
+        alertDialog.setMessage("New Button was clicked!");
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Who cares?",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 
     @Override
