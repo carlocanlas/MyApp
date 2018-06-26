2c42ca715bfe7e3d62b3652b2d0d7fa2549af389
Carlo Canlas <da.carlo.canlas@asia.ing.com>
2018-06-26 08:34:52 +0800

    toast demo challenge done

--- toastdemo/src/main/java/com/example/toastdemo/MainActivity.java
+++ toastdemo/src/main/java/com/example/toastdemo/MainActivity.java
@@ -2,8 +2,17 @@ package com.example.toastdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void triggerToast(View view) {
+        EditText name = (EditText) findViewById(R.id.name);
+
+        Toast.makeText(this, "Hello there " + name.getText().toString() + "!", Toast.LENGTH_SHORT).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
