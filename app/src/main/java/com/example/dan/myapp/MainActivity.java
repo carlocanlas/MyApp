13661f9c2036d44a1d33839e224cb5af3bdd9f2a
Carlo Canlas <da.carlo.canlas@asia.ing.com>
2018-06-26 08:12:59 +0800

    toast lesson

--- app/src/main/java/com/example/dan/myapp/MainActivity.java
+++ app/src/main/java/com/example/dan/myapp/MainActivity.java
@@ -6,22 +6,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view) {
-        Log.i("Info", "Button was pressed");
 
-        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
-        alertDialog.setTitle("Hey");
-        alertDialog.setMessage("New Button was clicked!");
-        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Who cares?",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                });
-        alertDialog.show();
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+
+        Log.i("Info", myTextField.getText().toString());
+
+        Toast.makeText(this, myTextField.getText().toString(), Toast.LENGTH_SHORT).show();
+
+//        AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
+//        alertDialog.setTitle("Hey");
+//        alertDialog.setMessage("New Button was clicked!");
+//        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Who cares?",
+//                new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        dialog.dismiss();
+//                    }
+//                });
+//        alertDialog.show();
     }
 
     @Override
